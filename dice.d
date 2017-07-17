9527692b68f4db8b40e1ad4587d8f7491ae40e51
Ray Kulhanek <kulhanek.5@wright.edu>
2017-07-17 18:42:14 -0400

    wtfpl

--- dice.d
+++ dice.d
@@ -1,3 +1,9 @@
+/* Copyright 2017 Ray Kulhanek
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See the COPYING file for more details.
+ */
+
 import std.stdio;
 import std.random;
 import std.string;
