eaffbbeac2a33b6325d0c31663d1fba799125997
Ray Kulhanek <kulhanek.5@wright.edu>
2018-03-16 18:47:44 -0400

    BUGFIX: d20 - -5 wouldn't have parsed right.  Also restructuring.

--- dice.d
+++ dice.d
@@ -10,6 +10,8 @@ import std.string;
 import std.conv;
 import std.regex;
 
+static string prev = "";
+
 extern (C) {
 	int nocbreak();
 	int noecho();
@@ -19,9 +21,9 @@ extern (C) {
 }
 
 string readline(string prompt) {
-	auto s = readline(prompt.ptr);
-	add_history(s);
-	return to!string(s);
+	auto s = readline(prompt.toStringz).to!string.strip;
+	if (s.length > 0) add_history(s.toStringz);
+	return s;
 }
 
 int roll(int n, int d) {
@@ -32,80 +34,91 @@ int roll(int n, int d) {
 	return sum;
 }
 
-void main() {
-	writef("Syntax example: sword 1d20 + 5, 1d8+2d6 (fire)+2 \n");
-	noecho();
-	raw();
-	string line;
-	
+void parseLine(string line) {
 	bool matches(string reg) {
 		return line.matchFirst(regex(reg, "m")).length() > 0;
 	}
 
 	int sign = 1;
-	string prev = "";
+	int sum = 0;
+	line = line.replaceAll(regex("[ \t\n]"), " ");
+	if (0 == line.length) {
+		line = prev;//repeat on empty string
+		writef("\033[1A");//move cursor up
+	}
+	prev = line;
+
+	while (line.length) {
+		if (matches("^[0-9]+[dD]") || matches("^[0-9]*[dD][0-9]+")) { //die code
+			uint n = 1, d = 6;
+			auto dcode = line.matchFirst(regex("^([0-9]*)[dD]([0-9]*)"));
+			if (dcode[1].length > 0) {
+				string s = dcode[1];
+				n = s.parse!uint;
+			}
+			if (dcode[2].length > 0) {
+				string s = dcode[2];
+				d = s.parse!uint;
+			}
+			auto r = roll(n, d);
+			writef("%s ", r);
+			sum += sign * r;
+			line = line[dcode[0].length..$];
+		}
+		else if (matches("^[0-9]+")) { // constant
+			auto n = line.parse!uint;
+			writef("%s ", n);
+			sum += sign * n;
+		}
+		else if (0 != line.indexOfAny("+-, ")) {//comment
+			auto offset = line.indexOfAny("+-, ");
+			writef("\x1b[38;5;245m%s\x1b[0m ", line[0..offset]);
+			line = line[offset..$];
+		}
+
+		if (line.length > 0) {
+			switch (line[0]) {
+				case '+':
+					sign = 1;
+					writef("+ ");
+					line = line[1..$];
+					break;
+				case '-':
+					writef("- ");
+					sign *= -1;
+					line = line[1..$];
+					break;
+				case ',':
+					writef("=\x1b[34;1m %s \x1b[0m, ", sum);
+					sum = 0;
+					line = line[1..$];
+					break;
+				case ' ':
+					line = line[1..$];
+					break;
+				default:
+					break;
+			}
+		}
+	}
+	writef("=\x1b[34;1m %s \x1b[0m\n", sum);
+}
+
+void main() {
+	writef("Syntax example: sword 1d20 + 5, 1d8+2d6 (fire)+2 \n");
+	//Sort of issue: the +s are unnecessary.  - signs toggle whether the next number will
+	//be added or subtracted, but it starts out as "add" whether or not a + appears. "sword 1d20 5, 1d8 2d6 (fire) 2"
+	//is equivalent to the above.  The comma is syntactically significant, though.
+	//5 - + 6 is equivalent to 5 + 6 or 5 6.  All this doesn't matter if stuff is entered in the canonical format, but
+	//it's still odd enough that I'm tempted to write a proper LR parser.
+	noecho();
+	raw();
+	
 	while (1) {
-		line = readline("> ") ~ " ";
+		string line = readline("> ") ~ " ";
 		if ("exit " == line || "quit " == line) break;
 		try {
-			int sum = 0;
-			line = line.replaceAll(regex("[ \t\n]"), " ");
-			if (matches("^ *$")) {
-				line = prev;//repeat on empty string
-				writef("\033[1A");//move cursor up
-			}
-			prev = line;
-			
-			sign = 1;
-			while (line.length) {
-				if (matches("^[0-9]*d[0-9]+")) { //die code
-					uint n = 1;
-					if ('d' != line[0]) {
-						n = line.parse!uint;
-					}
-					line = line[1..$];
-					auto d = line.parse!uint;
-					auto r = roll(n, d);
-					writef("%s ", r);
-					sum += sign * r;
-				}
-				else if (matches("^[0-9]+")) { // constant
-					auto n = line.parse!uint;
-					writef("%s ", n);
-					sum += sign * n;
-				}
-				else if (0 != line.indexOfAny("+-, ")) {//comment
-					auto offset = line.indexOfAny("+-, ");
-					writef("\x1b[38;5;245m%s\x1b[0m ", line[0..offset]);
-					line = line[offset..$];
-				}
-
-				if (line.length > 0) {
-					switch (line[0]) {
-						case '+':
-							sign = 1;
-							writef("+ ");
-							line = line[1..$];
-							break;
-						case '-':
-							writef("- ");
-							sign = -1;
-							line = line[1..$];
-							break;
-						case ',':
-							writef("=\x1b[34;1m %s \x1b[0m, ", sum);
-							sum = 0;
-							line = line[1..$];
-							break;
-						case ' ':
-							line = line[1..$];
-							break;
-						default:
-							break;
-					}
-				}
-			}
-			writef("=\x1b[34;1m %s \x1b[0m\n", sum);
+			parseLine(line);
 		}
 		catch (Throwable) {
 			writef("syntax error\n");
